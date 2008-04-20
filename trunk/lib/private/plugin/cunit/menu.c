26fe2b1ddd68b35f43e6f3a764a052a085827cb0
voidptrptr <voidptrptr@56febe37-ca3d-0410-bfee-2d6649462221>
2008-04-20 13:50:47 +0000

    Update utest/test.sh

--- trunk/lib/private/plugin/cunit/menu.c
+++ trunk/lib/private/plugin/cunit/menu.c
@@ -5,7 +5,7 @@ WPUBLIC int menu (void)
 {
     int choix = 0;
    
-    while (choix < 1 || choix > 3)
+    while (choix < 1 || choix > 4)
     {
         printf(" Running Tests :\n");
         printf("1 : Interactive Console Mode  \n");
@@ -14,19 +14,23 @@ WPUBLIC int menu (void)
         printf("Select a number ? ");
         scanf("%d", & choix);
     }   
+
     if (choix==2)
-         {
+      {
 		choix=0;
 		while (choix < 21 || choix > 23)
-   		 {
-      		  printf("\n Running Tests in mode basic :\n\n");
-      		  printf("21 : Failures and run summary are printed . \n");
-       		  printf("22 : Maximum output of run details. \n");
-       		  printf("23 : No output is printed except error messages.\n");  
-        	  printf("Select a number ? ");
-       		  scanf("%d", & choix);
-   		 }  
+          {
+            printf("\n Running Tests in mode basic :\n\n");
+            printf("21 : Failures and run summary are printed . \n");
+            printf("22 : Maximum output of run details. \n");
+            printf("23 : No output is printed except error messages.\n");  
+            printf("Select a number ? ");
+            scanf("%d", & choix);
+          }  
+      }
 
-          }
+    if (choix==4)
+      choix = 21;
+
     return choix;
 }   
